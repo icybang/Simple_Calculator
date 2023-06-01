4e9ce973fdc9cd33d616244c4d601ecfb2c079e8
abhinav <abhi.naik89@gmail.com>
2023-06-01 14:08:16 +0530

    updated calculator app

--- calculator/calculatorJS.js
+++ calculator/calculatorJS.js
@@ -1,31 +1,45 @@
-function calculate() {
 
-    validate();
+
+
+///////////////////////////////////////////////////////////////
+
+function calculate() {
+  validate();
 
   var num1 = parseFloat(document.getElementById("num1").value);
   var num2 = parseFloat(document.getElementById("num2").value);
+  var num3 = parseFloat(document.getElementById("num3").value);
+  var operator1 = document.getElementById("operator1").value;
+  var operator2 = document.getElementById("operator2").value;
 
-  var operationValue = document.querySelector('input[name="operation"]:checked').value;
+  var expression = num1 + operator1 + num2 + operator2 + num3;
+  var result = eval(expression);
 
-  var result;
-
-  if (operationValue == "addition") {
-    result = num1 + num2;
-  } else if (operationValue == "subtraction") {
-    result = num1 - num2;
-  } else if (operationValue == "multiplication") {
-    result = num1 * num2;
-  } else if (operationValue == "division") {
-    result = num1 / num2;
-  }
-
-  document.getElementById("result").innerHTML = '<div class="m-3 text-success fw-bold fs-4">Result: ' + result + '</div>';
+  document.getElementById("result").innerHTML =
+    '<div class="m-3 text-success fw-bold fs-4">Result: ' + result + "</div>";
 }
 
+
+//////////////////////////////////////////////////////
+
 function validate(){
+
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
     var num3 = parseFloat(document.getElementById("num3").value);
+    var operator1 = document.getElementById("operator1").value;
+    var operator2 = document.getElementById("operator1").value;
+    
+    if (operator1 === "") {
+      document.getElementById("op1Er").innerHTML="Please select operator";
+    } else{
+      document.getElementById("op1Er").innerHTML="";
+    }
+    if (operator2 === "") {
+      document.getElementById("op2Er").innerHTML="Please select operator";
+    } else{
+      document.getElementById("op2Er").innerHTML="";
+    }
 
     if (isNaN(num1)){
     document.getElementById("num1Er").innerHTML="Please Enter a Valid Number"
