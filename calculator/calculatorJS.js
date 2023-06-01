1ef3fbc7f22f05226112a144dbdf706c1964ed33
abhinav <abhi.naik89@gmail.com>
2023-06-01 11:11:29 +0530

    added calculator folder

---
+++ calculator/calculatorJS.js
@@ -0,0 +1,41 @@
+function calculate() {
+
+    validate();
+
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+
+  var operationValue = document.querySelector('input[name="operation"]:checked').value;
+
+  var result;
+
+  if (operationValue == "addition") {
+    result = num1 + num2;
+  } else if (operationValue == "subtraction") {
+    result = num1 - num2;
+  } else if (operationValue == "multiplication") {
+    result = num1 * num2;
+  } else if (operationValue == "division") {
+    result = num1 / num2;
+  }
+
+  document.getElementById("result").innerHTML = '<div class="m-3 text-success fw-bold fs-4">Result: ' + result + '</div>';
+}
+
+function validate(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operation = document.querySelector('input[name="operation"]:checked');
+
+    if (isNaN(num1)){
+    document.getElementById("num1Er").innerHTML="Please Enter a Valid Number"
+    } else document.getElementById("num1Er").innerHTML=""
+
+    if (isNaN(num2)){
+    document.getElementById("num2Er").innerHTML="Please Enter a Valid Number"
+    } else document.getElementById("num2Er").innerHTML=""
+
+    if (!operation) {
+    document.getElementById("opr").innerHTML="Please select Operation"
+    } else document.getElementById("opr").innerHTML=""
+}
